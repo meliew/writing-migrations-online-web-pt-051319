b5924e64232f348b660071a19f1b0259233c3305
Melissa Williams <melissa@brandmeetscopy.com>
2019-07-27 23:09:49 +0000

    Automatically backed up by Learn

--- db/migrate/02_add_grade_and_birthdate_to_students.rb
+++ db/migrate/02_add_grade_and_birthdate_to_students.rb
@@ -1,9 +1,6 @@
 class AddGradeAndBirthdateToStudents < ActiveRecord::Migration[5.1]
 
   def change
-    add_column(grade, birthdate) :students do |t|
-      t.integer :grade
-      t.string :birthdate
+    add_column :grade, :birthdate:
     end
   end
-end
