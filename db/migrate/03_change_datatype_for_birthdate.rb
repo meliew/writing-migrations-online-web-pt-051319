8680304fb9cff3e2c041bb5060ad729f63ef585e
Melissa Williams <melissa@brandmeetscopy.com>
2019-07-27 23:14:00 +0000

    Automatically backed up by Learn

--- db/migrate/03_change_datatype_for_birthdate.rb
+++ db/migrate/03_change_datatype_for_birthdate.rb
@@ -1,7 +1,7 @@
 class ChangeDatatypeForBirthdate < ActiveRecord::Migration[5.1]
 
   def change
-    add_column :students, :birthdate, :string
+    add_column :students, :birthdate, :datetime
   end
 
 end
